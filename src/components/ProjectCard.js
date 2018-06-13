4d97762ba0f28939513c48232018f49f734adee2
Student <example@example.com>
2018-06-13 10:03:43 -0700

    add to hard coded projectInfo array, create css styles for image property iun projectInfo array

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ function ProjectCard({project}) {
   return (
     <div className="project">
     <div className="project-description"><h3>{project.name}</h3><p>{project.description}</p></div>
-    <div className="project-pic">
+    <div className={project.image}>
 </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,12 +6,22 @@ const projectInfo = [
   {
     name: "Better Doctor API",
     description: "A project utilizing Better Doctor's API to help people find doctor's fitting to their needs.",
-    image: './doctor-code.JPG'
+    image: 'project-pic project1'
   },
   {
-    name: "Other Cool Thing",
-    description: "A project utilizing Better Doctor's API to help people find doctor's fitting to their needs.",
-    image: './doctor-code.JPG'
+    name: "Backcountry Mock-up",
+    description: "A CSS and HTML clone of Backcountry.com's frontpage.",
+    image: 'project-pic project2'
+  },
+  {
+    name: "Smith Rock Route Finder",
+    description: "A project built in Angular with CRUD capabilities and Firebase as a database.",
+    image: 'project-pic project3'
+  },
+  {
+    name: "Common Good",
+    description: "A shared-space website mock-up built from the ground up in Sketch and utilizing SASS.",
+    image: 'project-pic project4'
   }
 ];
 
